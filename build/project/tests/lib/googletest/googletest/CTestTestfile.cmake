c3a0dfa817defca0db34afd252df6d73e3f52d75
Dellvin <dellvin.black@gmail.com>
2020-03-20 21:31:46 +0300

    remove useless

--- build/project/tests/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dellvin/Desktop/iz2-branchForTest 2/project/tests/lib/googletest/googletest
-# Build directory: /home/dellvin/Desktop/iz2-branchForTest 2/build/project/tests/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
